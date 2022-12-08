17294a064c4ad0451880d6964051a32aedc42317
Benny <benny.le890@gmail.com>
2022-12-08 00:45:03 -0500

    finished App.js front end

--- client/src/App.js
+++ client/src/App.js
@@ -21,18 +21,23 @@ const httpLink = createHttpLink({
   uri: '/graphql',
 });
 
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <Router>
-      <>
+    <ApolloClient client={client}>
+      <Router>
         <Navbar />
         <Switch>
           <Route exact path='/' component={SearchBooks} />
           <Route exact path='/saved' component={SavedBooks} />
           <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
         </Switch>
-      </>
-    </Router>
+      </Router>
+    </ApolloClient>
   );
 }
 
